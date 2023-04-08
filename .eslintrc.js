6757e67923e46e7a938c8b109d9a87dd0124b72a
nirmaanlabs <nirmaan.labs@gmail.com>
2023-04-08 19:21:45 +0530

    add prettier and es lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,42 +1,49 @@
 module.exports = {
-  extends: 'universe/native',
+  extends: "universe/native",
   overrides: [
     {
+      files: ["*.ts", "*.tsx"],
       rules: {
-        'jsx-quotes': ['warn', 'prefer-single'],
-        'no-unused-var': 'error',
-        'no-console': ['error', { allow: ['warn', 'error', 'info'] }],
+        quotes: ["error", "double", { avoidEscape: true }],
+        "no-unused-vars": "error",
+        "no-console": ["error", { allow: ["warn", "error", "info"] }],
 
         // this is for sorting WITHIN an import
-        'sort-imports': [
-          'error',
+        "sort-imports": [
+          "error",
           { ignoreCase: true, ignoreDeclarationSort: true },
         ],
+        "prettier/prettier": [
+          "error",
+          {
+            endOfLine: "auto",
+          },
+        ],
         // this is for sorting imports
-        'import/order': [
-          'error',
+        "import/order": [
+          "error",
           {
             groups: [
-              ['external', 'builtin'],
-              'internal',
-              ['sibling', 'parent'],
-              'index',
+              ["external", "builtin"],
+              "internal",
+              ["sibling", "parent"],
+              "index",
             ],
             pathGroups: [
               {
-                pattern: '@(react|react-native)',
-                group: 'external',
-                position: 'before',
+                pattern: "@(react|react-native)",
+                group: "external",
+                position: "before",
               },
               {
-                pattern: '@src/**',
-                group: 'internal',
+                pattern: "@src/**",
+                group: "internal",
               },
             ],
-            pathGroupsExcludedImportTypes: ['internal', 'react'],
-            'newlines-between': 'always',
+            pathGroupsExcludedImportTypes: ["internal", "react"],
+            "newlines-between": "always",
             alphabetize: {
-              order: 'asc',
+              order: "asc",
               caseInsensitive: true,
             },
           },
