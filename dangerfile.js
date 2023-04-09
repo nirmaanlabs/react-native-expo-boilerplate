ebb72cc810a036dad1937add5d154565e8a5a0c3
nirmaanlabs <nirmaan.labs@gmail.com>
2023-04-09 20:21:17 +0530

    change import for path

--- dangerfile.js
+++ dangerfile.js
@@ -1,11 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { danger, message } from "danger";
-import path from "path";
 import { dangerReassure } from "reassure";
 
-const modifiedMD = danger.git.modified_files.join("- ");
-
-message("Changed Files in this PR: \n - " + modifiedMD);
+const path = require("path");
 
 dangerReassure({
   inputFilePath: path.join(__dirname, "./.reassure/output.md"),
