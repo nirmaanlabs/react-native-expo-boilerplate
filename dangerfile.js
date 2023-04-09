4ea90bce5cd762432b3da16662344143a6b2d0c4
nirmaanlabs <nirmaan.labs@gmail.com>
2023-04-09 22:47:47 +0530

    add some comments in file

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { message } from "danger";
 import { readFileSync, writeFileSync } from "fs";
 import { dangerReassure } from "reassure";
 
@@ -8,6 +9,8 @@ const file = readFileSync(
   path.join(__dirname, "./.reassure/current.perf"),
   "utf8"
 );
+
+message("Changed Files in this PR: \n - ");
 
 writeFileSync(path.join(__dirname, "./.reassure/output.md"), "file");
 
