10bcc3a1e8113537f97290b218cb651da08a6579
nirmaanlabs <nirmaan.labs@gmail.com>
2023-04-09 20:33:40 +0530

    comment unused code

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,8 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { danger, message } from "danger";
-import { dangerReassure } from "reassure";
+// import { dangerReassure } from "reassure";
 
-const path = require("path");
+// const path = require("path");
 
 const modifiedMD = danger.git.modified_files.join("- ");
 message("Changed Files in this PR: \n - " + modifiedMD);
