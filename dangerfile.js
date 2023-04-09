571af75ac7099451679c00890e25703483df0d46
nirmaanlabs <nirmaan.labs@gmail.com>
2023-04-09 20:28:29 +0530

    add danger js modified files

--- dangerfile.js
+++ dangerfile.js
@@ -1,7 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import { danger, message } from "danger";
 import { dangerReassure } from "reassure";
 
 const path = require("path");
+
+const modifiedMD = danger.git.modified_files.join("- ");
+message("Changed Files in this PR: \n - " + modifiedMD);
 
 dangerReassure({
   inputFilePath: path.join(__dirname, "./.reassure/output.md"),
